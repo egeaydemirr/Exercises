f74e9e31223dec621bf8b5131c8fc96cfd564c8b
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-15 10:45:24 +0300

    Initial Commit

---
+++ Exercises/ExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExercisesApp.swift
+//  Exercises
+//
+//  Created by Ege Aydemir on 15.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
