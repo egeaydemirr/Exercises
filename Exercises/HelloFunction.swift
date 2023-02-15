34525d06b5713ae51538861d435e79d258594ab8
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-15 10:59:40 +0300

    HelloWorld class and function

--- Exercises/HelloFunction.swift
+++ Exercises/HelloFunction.swift
@@ -5,4 +5,12 @@
 //  Created by Ege Aydemir on 15.02.2023.
 //
 
+
+// MARK: - 1st Exersize: Create Hello World Function
 import Foundation
+
+class helloWorld: ObservableObject {
+    func helloWorld() -> String {
+        return "Hello World"
+    }
+}
